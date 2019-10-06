8f9a4056f1ec8de8e5d5cf2ce69e8898e73d6660
rachana <rachanadev1210@gmail.com>
2019-10-06 14:08:40 +0530

    added login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component1.html',
+  styleUrls: ['./login.component1.scss']
+})
+export class LoginComponent1 implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
